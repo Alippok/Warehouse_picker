dfb1cebe4e493b3d4c14ad7784bab06563611eb1
A_Lippok <alippok@hotmail.co.uk>
2016-02-19 14:06:33 +0000

    Passes first test to determine what a hash turned into an array looks like

---
+++ warehouse_picker.rb
@@ -0,0 +1,5 @@
+def hash_to_array(hash)#for identifying position and distance between 
+   return hash.to_a#positions
+  
+
+end
--- warehouse_picker_specs.rb
+++ warehouse_picker_specs.rb
@@ -3,9 +3,21 @@ require_relative("warehouse_picker.rb")
 
 class TestWarehousePicker < MiniTest::Test
 
+def setup
 
+  @my_hash = {
+     row_a: "Pickles",
+     row_b: "Cheese",
+     row_c: "Ham"
+    
+    }
+end
 
-
+def test_hash_to_array()
+  result = hash_to_array(@my_hash)
+  string = [[:row_a , "Pickles"], [:row_b, "Cheese"], [:row_c, "Ham"]]
+  assert_equal(string, result)
+end
 
 
 
